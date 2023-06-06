10fd360d51d02011db24e4b25603e852296cfb6a
Shiyu Li <lishiyu8866@gmail.com>
2023-06-06 12:53:51 +0800

    update program 1

--- Algorithms and Data Structures/Program.cs
+++ Algorithms and Data Structures/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Program 1: Max number:
+//On^2
 List<List<int>> numbers = new List<List<int>>
 {
     new List<int> { 1, 5, 3 },
     new List<int> { 9, 7, 3, -2 },
-    new List<int> { 2, 1, 2 }
+    new List<int> { 2, 1, 2 },
+    new List<int> { -1 }
 };
 List<int> MaxNumbers = new List<int>();
 int maxNumberInList = numbers[0][0];
@@ -11,15 +13,17 @@ if (numbers.Count > 0)
 {
     foreach(List<int> list in numbers) 
     {
-        foreach(int number in list)
+        maxNumberInList = list[0]; // if there's any negative number, it also can be output
+        foreach (int number in list)
         {
             if (number > maxNumberInList)
             {
                 maxNumberInList = number;
-                MaxNumbers.Add(maxNumberInList);
-            }        
+                
+                
+            }       
         }
-        maxNumberInList = 0;
+        MaxNumbers.Add(maxNumberInList);
     }
 } else
 {
@@ -28,5 +32,13 @@ if (numbers.Count > 0)
 
 for (int i = 0; i < MaxNumbers.Count; i++)
 {
-    Console.WriteLine($"List {i + 1} has a maximum of {MaxNumbers[i]}.");
+    Console.Write($" List {i + 1} has a maximum of {MaxNumbers[i]}. ");
 }
+Console.WriteLine();
+
+
+
+//Program 2:  HighestGrade:
+
+List<List<int>> grades = new List<List<int>>();
+List<int> Grades1 = new List<int> { 85, 92, 67, 94, 94 };
